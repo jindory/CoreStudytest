7a94821c70e1bb05f85a99e8eb5ad2fda0bc8835
jindory <laonjay@gmail.com>
2022-06-26 22:52:09 +0900

    발표 끝ㅎ-ㅎ

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 // TODO: cors를 적용합니다.
+app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
+app.use(express.json());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
 
 const port = 3001;
 const discussionsRouter = require('./router/discussions'); 
 // TODO: /discussions 경로로 라우팅합니다. 
+app.use('/discussions', discussionsRouter);
 // 라우팅 앞에 주소 를 쳤을경우 뒤에 오는 값의 있는 애들을 참조해라
 
-  // TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
-
+// TODO: 서버 상태 확인을 위해 상태 코드 200으로 응답합니다.
+app.get('/',  (req, res) => {
+  res.status(200).send(`Welcome, study core 🎉`);
+  throw ''
+});
 
 
 const server = app.listen(port, () => {
--- controller/index.js
+++ controller/index.js
@@ -1,23 +1,61 @@
 const { agoraStatesDiscussions } = require("../repository/discussions");
 const discussionsData = agoraStatesDiscussions;
 const discussionsController = {
+
+  // TODO: 모든 discussion 목록을 조회
   findAll: (req, res) => {
-    // TODO: 모든 discussions 목록을 응답합니다.
+    if(req.query){
+      return res.status(200).json(discussionsData);
+    }
   },
-    
-  findById: (req, res) => { 
   
+  // TODO: discussion 하나를 조회
+  findById: (req, res) => { 
+    if(req.params){
+      let list = discussionsData.find((list) => {
+       return req.params.id === String(list.id)
+      })
+      return res.status(200).send(list);
+    }
   },
+
+  // ADVANCED: discussion 하나를 생성
   createOne: (req, res) => {
-
+    const { username, title, content } = req.body;
+    if(req.query){
+      const id = parseInt(Math.random() * 10000)
+      const newDiscussion = {
+        id,
+        username,
+        title,
+        content,
+        createdAt: new Date().toISOString()
+      };
+      discussionsData.unshift(newDiscussion);
+      return res.status(200).send('🎉 데이터 추가');
+    }
   },
 
+  // ADVANCED: discussion 하나를 수정
   updateById: (req, res) => {
-
+    if(req.params){
+      const idx = discussionsData.findIndex((d) => d.id === Number(req.params.id));
+      const updated = {
+        ...discussionsData[idx], ...req.body,
+        createdAt: new Date().toISOString()
+      };
+      discussionsData.splice(idx, 1, updated);
+      return res.status(200).send('🎉 데이터 수정');
+    }
   },
 
+  // ADVANCED: discussion 하나를 삭제
   deleteById: (req, res) => {
-
+    if(req.params){
+      const idx = discussionsData.findIndex((d) => d.id === Number(req.params.id));
+      discussionsData.splice(idx, 1);
+      return res.status(200).send('🎉 데이터 삭제');
+    }
   },
 }; 
 
